3c8ead1f19827605c87a47b6370891f89ba3f36f
Jordan Bloodworth <jlbworth@gmail.com>
2020-05-06 20:48:13 -0500

    First Upload

---
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using leave_management.Contracts;
+using leave_management.Data;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            // return _db.LeaveTypes.ToList();
+            var leaveAllocations = _db.LeaveAllocations.ToList();
+            return leaveAllocations;
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            // _db.LeaveTypes.FirstOrDefault()
+            var leaveAllocations = _db.LeaveAllocations.Find(id);
+            return leaveAllocations;
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Update(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Remove(entity);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0; //(if changes > 0, return)
+        }
+
+        public ICollection<LeaveAllocation> GetEmployeesLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
